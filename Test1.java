6528483ceb9d36149ee812861f0fbbb1aa547346
Yerreddula Vinnutna <129519608+vinuthnar@users.noreply.github.com>
2024-06-24 21:09:41 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,10 @@
+class Test1{
+public static void main(String args[]){
+if(args.length<2){
+    System.out.print("error");
+}
+else{
+System.out.print(args[0] + " Technologies " + args[1]);
+}
+}
+}
---
+++ Test3.java
@@ -0,0 +1,9 @@
+class Test3{
+public static void main(String args[]){
+int a=Integer.parseInt(args[0]);
+int b=Integer.parseInt(args[1]);
+int c= a+b;
+System.out.print("sum ="+ c);
+}
+}
+
